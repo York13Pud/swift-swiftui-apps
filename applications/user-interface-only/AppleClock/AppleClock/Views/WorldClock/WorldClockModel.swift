9d9ee3e65c5cc8d3054f8f14dce15eb2b970133a
Dev Neil A <16964133+York13Pud@users.noreply.github.com>
2024-06-04 10:08:55 +0100

    Updated model and added sample data

--- applications/user-interface-only/AppleClock/AppleClock/Views/WorldClock/WorldClockModel.swift
+++ applications/user-interface-only/AppleClock/AppleClock/Views/WorldClock/WorldClockModel.swift
@@ -1,3 +1,14 @@
 //
 
 import Foundation
+
+struct WorldClock: Identifiable {
+    let id: UUID = UUID()
+    var location: String
+    var time: String
+    var timezone: String
+}
+
+// Two sample records that can be usedL
+let zoneOne = WorldClock(location: "London", time: "01:30", timezone: "+0")
+let zoneTwo = WorldClock(location: "Paris", time: "02:30", timezone: "+1")

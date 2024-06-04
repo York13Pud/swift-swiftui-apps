8663a1d65d2607ca416d5ce611980c5dfde61837
Dev Neil A <16964133+York13Pud@users.noreply.github.com>
2024-06-04 13:47:17 +0100

    Completed world clocks tab and views

--- applications/user-interface-only/AppleClock/AppleClock/Views/WorldClock/WorldClockLocationView.swift
+++ applications/user-interface-only/AppleClock/AppleClock/Views/WorldClock/WorldClockLocationView.swift
@@ -3,11 +3,47 @@
 import SwiftUI
 
 struct WorldClockLocationView: View {
+    // Check to see if the view is in editMode:
+    @Environment(\.editMode) private var editMode
+    
+    // Define required constants:
+    let location: String
+    let time: String
+    let timezone: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .lastTextBaseline) {
+            VStack(alignment: .leading) {
+                Text("Today, \(timezone)HRS")
+                    .font(.system(size: 21))
+                    .foregroundStyle(.gray)
+                Text(location)
+                    .font(.largeTitle)
+            }
+            // Set the text to scale down when text is too long to fit on one line
+            .minimumScaleFactor(0.01)
+            .lineLimit(1)
+            
+            Spacer()
+            
+            if editMode?.wrappedValue.isEditing == false {
+                VStack(alignment: .trailing) {
+                    Text(time)
+                        .fontWeight(.light)
+                        .font(.system(size: 56))
+                }
+            } else {
+                
+            }
+        }
+        .padding([.top, .bottom], 15)
+    
+        // Align the text so that it is inline with the edit button and navbar title:
+        .listRowInsets(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
     }
 }
 
 #Preview {
-    WorldClockLocationView()
+    WorldClockLocationView(location: "Lodon", time: "12:34", timezone: "+0")
+        .environment(\.colorScheme, .dark) // Added to force dark mode in preview window.
 }
--- applications/user-interface-only/AppleClock/AppleClock/Views/WorldClock/WorldClockView.swift
+++ applications/user-interface-only/AppleClock/AppleClock/Views/WorldClock/WorldClockView.swift
@@ -11,84 +11,57 @@ struct WorldClockView: View {
     func removeRows(at offsets: IndexSet) {
         locations.remove(atOffsets: offsets)
     }
-       
+    
     var body: some View {
         
         NavigationStack {
             ZStack {
                 // Set the background colour and ignore safe area:
                 backgroundColour
-                .ignoresSafeArea()
+                    .ignoresSafeArea()
                 
                 // Put the main content of the view here:
                 List {
                     ForEach(locations, id: \.id) { location in
-                        HStack(alignment: .lastTextBaseline) {
-                            VStack(alignment: .leading) {
-                                Text("Today, \(location.timezone)HRS")
-                                    .font(.system(size: 21))
-                                    .foregroundStyle(.gray)
-                                Text(location.location)
-                                    .font(.largeTitle)
-                            }
-                            // Set the text to scale down when text is too long to fit on one line
-                            .minimumScaleFactor(0.01)
-                            .lineLimit(1)
-                            
-                            Spacer()
-                            
-                            VStack(alignment: .trailing) {
-                                Text(location.time)
-                                    .fontWeight(.light)
-                                    .font(.system(size: 56))
-                            }
-                            
-                        }
-                        .padding([.top, .bottom], 15)
-                        
-                        // Align the text so that it is inline with the edit button and navbar title:
-                        .listRowInsets(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
-                        
-                        
-                    }.onDelete(perform: removeRows)
-                    
-                        
+                        WorldClockLocationView(location: location.location, time: location.time, timezone: location.timezone)
                     }
-                    .listStyle(.plain)
+                    .onDelete(perform: removeRows)
+                    .onMove { locations.move(fromOffsets: $0, toOffset: $1) }
                 }
-                
-                // Sets the title of the view
-                .navigationTitle(title)
-                .overlay() {
-                    if locations.isEmpty {
-                        ContentUnavailableView {
-                            // No title
-                        } description: {
-                            Text("No World Clocks")
-                                .font(.system(size: 28).weight(.light))
-                        }
+                .listStyle(.plain)
+            }
+            
+            // Sets the title of the view
+            .navigationTitle(title)
+            
+            // Check to see if the locations array is empty:
+            .overlay() {
+                if locations.isEmpty {
+                    ContentUnavailableView {
+                        // No title
+                    } description: {
+                        Text("No World Clocks")
+                            .font(.system(size: 28).weight(.light))
                     }
                 }
+            }
+            
+            // Specify any buttons that need to be added to the navigation bar:
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    EditButton()
+                }
                 
-                // Specify any buttons that need to be added to the navigation bar:
-                .toolbar {
-                    ToolbarItem(placement: .topBarLeading) {
-                        EditButton()
-                        //                        Button("Edit") {
-                        //                            // Add edit function code here
-                        //                        }
-                    }
-                    
-                    ToolbarItem(placement: .topBarTrailing) {
-                        Button("", systemImage: "plus") {
-                            locations.append(zoneOne)
-                            locations.append(zoneTwo)
-                        }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("", systemImage: "plus") {
+                        locations.append(zoneOne)
+                        locations.append(zoneTwo)
                     }
                 }
             }
         }
     }
+}
 
 
 #Preview {

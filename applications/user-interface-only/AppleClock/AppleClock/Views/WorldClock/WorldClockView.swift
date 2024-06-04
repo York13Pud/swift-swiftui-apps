55c8eb46a0bd4ff176206decff30141a3e1c7925
Dev Neil A <16964133+York13Pud@users.noreply.github.com>
2024-06-04 10:09:33 +0100

    Updated list to use a foreach loop

--- applications/user-interface-only/AppleClock/AppleClock/Views/WorldClock/WorldClockView.swift
+++ applications/user-interface-only/AppleClock/AppleClock/Views/WorldClock/WorldClockView.swift
@@ -5,9 +5,9 @@ import SwiftUI
 struct WorldClockView: View {
        
     let title: String
-    
+    @State private var locations = [WorldClock]()
+       
     var body: some View {
-//        var locations: [String]
         
         NavigationStack {
             ZStack {
@@ -17,77 +17,35 @@ struct WorldClockView: View {
                 
                 // Put the main content of the view here:
                 List {
-                    HStack(alignment: .lastTextBaseline) {
-                        VStack(alignment: .leading) {
-                            Text("Today, +0HRS")
-                                .font(.system(size: 21))
-                                .foregroundStyle(.gray)
-                            Text("London")
-                                .font(.largeTitle)
+                    ForEach(locations, id: \.id) { location in
+                        HStack(alignment: .lastTextBaseline) {
+                            VStack(alignment: .leading) {
+                                Text("Today, \(location.timezone)HRS")
+                                    .font(.system(size: 21))
+                                    .foregroundStyle(.gray)
+                                Text(location.location)
+                                    .font(.largeTitle)
+                            }
+                            // Set the text to scale down when text is too long to fit on one line
+                            .minimumScaleFactor(0.01)
+                            .lineLimit(1)
+                            
+                            Spacer()
+                            
+                            VStack(alignment: .trailing) {
+                                Text(location.time)
+                                    .fontWeight(.light)
+                                    .font(.system(size: 56))
+                            }
                         }
-                        // Set the text to scale down when text is too long to fit on one line
-                        .minimumScaleFactor(0.01)
-                        .lineLimit(1)
+                        .padding([.top, .bottom], 15)
                         
-                        Spacer()
+                        // Align the text so that it is inline with the edit button and navbar title:
+                        .listRowInsets(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
                         
-                        VStack(alignment: .trailing) {
-                            Text("12:34")
-                                .fontWeight(.light)
-                                .font(.system(size: 56))
-                        }
                     }
-                    .padding([.top, .bottom], 15)
-                    
-                    // Align the text so that it is inline with the edit button and navbar title:
-                    .listRowInsets(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
-
-//                    .overlay(Rectangle().frame(width: nil,
-//                                               height: 0.5,
-//                                               alignment: .top)
-//                                        .foregroundColor(Color.gray),
-//                                                         alignment: .top)
-//                    
-//                    .overlay(Rectangle().frame(width: nil,
-//                                               height: 0.6,
-//                                               alignment: .bottom)
-//                                        .foregroundColor(Color.gray),
-//                                                         alignment: .bottom)
-                    
-                    HStack(alignment: .lastTextBaseline) {
-                        VStack(alignment: .leading) {
-                            Text("Today, +0HRS")
-                                .font(.system(size: 21))
-                                .foregroundStyle(.gray)
-                            Text("London")
-                                .font(.largeTitle)
-                        }
-                        // Set the text to scale down when text is too long to fit on one line
-                        .minimumScaleFactor(0.01)
-                        .lineLimit(1)
-                        
-                        Spacer()
-                        
-                        VStack(alignment: .trailing) {
-                            Text("12:34")
-                                .fontWeight(.light)
-                                .font(.system(size: 56))
-                        }
-                    }
-                    .padding([.top, .bottom], 15)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
-                    .listRowSeparatorTint(.gray)
+                    .listStyle(.plain)
                 }
-                .listStyle(.plain)
-
-//                List {
-//                    ForEach((1...18), id: \.self) {
-//                        Text("\(title) \($0)")
-//                            .foregroundStyle(.white)
-//                            .background(.green)
-//                    }
-//                    .listRowBackground(backgroundColour)
-//                }
                 
                 // Sets the title of the view
                 .navigationTitle(title)
